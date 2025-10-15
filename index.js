b791666b196ed69c4e12f58c3dd6e7a3c3565800
Lenovo <9.3razzinayyuri@gmail.com>
2025-10-15 11:45:04 +0700

    Connection to MySql

--- index.js
+++ index.js
@@ -7,3 +7,11 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 })
 
+const db = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    port: '3309',
+    password: 'CodingItuMenyenangkan29_',
+    database: 'mahasiswa',
+})
+
